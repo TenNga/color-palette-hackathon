743d41a8fa83f257e8127c7ed85b4459d35b7bed
tennga <tenngawang6080@gmail.com>
2020-07-22 15:09:59 -0400

    final version

--- src/App.js
+++ src/App.js
@@ -1,58 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 
 import ImageUploader from 'react-images-upload';
-import { usePalette } from 'react-palette';
 import {Palette} from 'react-palette';
+import styled from 'styled-components';
 
 import './App.css';
 
-class App extends React.Component {
- 
-  state = {
-    pictures: "",
-    url: ""
-  }
+const App = () => {
 
-  onDrop = (picture) => {
-    this.setState({
-        pictures: picture
-    });
+  const [pictures, setPicture] = useState([]);
+
+  const AppContainer = styled.div`
+    margin: 0 2rem;
+  `;
+
+  const Header = styled.h1`
+    text-align: center;
+    background-color: #448db8;
+    color: #f2f2f2;
+    padding: 1rem;
+    font-size: 3rem;
+  `;
+
+  const PaletteContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4,1fr);
+    margin: 1rem 0;
+  `;
+
+  const SingleColor = styled.div`
+    padding: 2rem 0rem;
+    text-align: center;
+    :hover {
+      font-weight: bolder;
+    }
+  `;
+
+  //execute everytime image upload
+  const onDrop = (picture) => setPicture([...picture]);
+
+  const colorClicked = (e) => {
+    const text = e.target.textContent;
+    const textholder = document.createElement('textarea');
+    textholder.value = text;
+    document.body.appendChild(textholder);
+    textholder.select();
+    document.execCommand('copy');
+    alert("Copied the text: " + textholder.value);
+    document.body.removeChild(textholder);
+    
+  }
+  const fileStyled = {
+    backgroundColor: "#42bec9"
   }
   
-  render(){
-    
-    console.log("Picture: ", this.state.pictures[0]);
-    // if(this.state.pictures.length > 0){
-    //   // const { data, loading, error } = usePalette(URL.createObjectURL(this.state.pictures[0]));
-    //   const path = URL.createObjectURL(this.state.pictures[0])
-    //   console.log("URL: ", path)
-    // } 
+    console.log("Picture: ", pictures);
     return (
-      <div className="App">
-        <ImageUploader
-                  withIcon={true}
-                  buttonText='Choose images'
-                  onChange={this.onDrop}
-                  imgExtension={['.jpg', '.gif', '.png', '.gif']}
-                  maxFileSize={5242880}
-                  withPreview={true}
-        />
-        {this.state.pictures?
-        // <img src={URL.createObjectURL(this.state.pictures[0])}/>
-        <Palette src={URL.createObjectURL(this.state.pictures)}>
-        {(palette) => (
-          <div style={{ color: palette.data.vibrant}}>
-            Text with the vibrant color {palette.darkVibrant}
-          </div>
-        )}
-      </Palette>
-         : null}
-
-      
+      <div style={{marginLeft: "1rem", marginRight: "1rem"}}>
+        <Header>
+          Color Palette Generator From Image
+        </Header>
+        {/* // logic to display color palette only after image upload */}
+        {
+          pictures.length > 0?
+          <Palette src={URL.createObjectURL(pictures[pictures.length-1])}>
+            {(palette) => (
+              <PaletteContainer>
+                <SingleColor onClick={colorClicked} style={{ backgroundColor: palette.data.vibrant}}>
+                  {palette.data.vibrant}
+                </SingleColor>
+                <SingleColor onClick={colorClicked} style={{ backgroundColor: palette.data.muted}}>
+                  {palette.data.muted}
+                </SingleColor>
+                <SingleColor onClick={colorClicked} style={{ backgroundColor: palette.data.lightMuted}}>
+                  {palette.data.lightMuted}
+                </SingleColor>
+                <SingleColor onClick={colorClicked} style={{ backgroundColor: palette.data.darkMuted}}>
+                  {palette.data.darkMuted}
+                </SingleColor>
+              </PaletteContainer>
+            
+            )}
+          </Palette> : null
+        }  
+          <ImageUploader
+                    withIcon={true}
+                    buttonText='Choose images'
+                    onChange={onDrop}
+                    imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                    maxFileSize={5242880}
+                    withPreview={true}
+                    fileContainerStyle ={fileStyled}
+          />
       </div>
     );
-  }
+
 }
 
 export default App;
--- src/ColorPalette.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ColorPalette = () => {
-    return(
-        <h1>Color Palette</h1>
-    )
-}
-
-export default ColorPalette;
